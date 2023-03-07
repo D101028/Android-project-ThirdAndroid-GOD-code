dc447546016c1fd32aba3541af165ac9c301e0ee
D101028 <Damont101028@gmail.com>
2023-03-07 22:31:36 +0800

    message

--- app/src/main/java/com/example/thirdandroid/MainActivity.java
+++ app/src/main/java/com/example/thirdandroid/MainActivity.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void open_browser(View view) {
-        EditText inputBlock = (EditText) findViewById(R.id.GodCode);
+        EditText inputBlock = (EditText) findViewById(R.id.GodCodeN);
         String user_input = inputBlock.getText().toString();
         String url = "https://nhentai.net/g/" + user_input + "/";
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
